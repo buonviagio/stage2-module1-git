f9eabc15f008bd06ff985bfca0d86590be67fab7
Shapovalov Dmytro <buonviagio@gmail.com>
2023-04-09 11:33:14 +0300

    My second commit Stage2 Module#1 Task#Git

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/buonviagio/stage2-module1-gitTask";
+    public static String REPO_LINK = "https://github.com/buonviagio/module1-task-git";
 }
